b9d0e5184c3068927e5ad069266ef2ad6399b5dd
ChrisJPena <chrispclicks@gmail.com>
2023-01-11 21:40:06 -0500

    Created a drawing, finalized the algorithm, created the required classes

---
+++ algorithim.py
@@ -0,0 +1,11 @@
+# Steps
+# 1. Intro
+# 2. Display the rules
+# 3. ask how many human players will be playing
+# 4. show the options
+# 5. choose your move
+# 6. prints your result
+# 7. prints AI/Human choice
+# 8. Result per rules
+# 9. present next round first player to win twice wins
+# 10. repeat 4-7 until someone wins twice
